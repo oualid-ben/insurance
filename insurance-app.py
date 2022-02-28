12209b70b7d31ea8ac1b72e4e24e628216a7269a
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-02-28 18:56:54 +0100

    Update insurance-app.py

--- insurance-app.py
+++ insurance-app.py
@@ -26,7 +26,7 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        model  = st.sidebar.selectbox('Model',('KNN'))
+        model  = st.sidebar.selectbox('Model',('KNN', 'Random Forest'))
         number_of_vehicles_involved = st.sidebar.slider('number of vehicles involved', 1,3,2)
         witnesses = st.sidebar.slider('Witnesses', 0,3,1)
         data = {'model': model,
