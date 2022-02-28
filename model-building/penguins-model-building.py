2dce8b3e013474f3dd600656ec381115384b7098
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-02-28 17:49:38 +0100

    mise à jour

--- model-building/penguins-model-building.py
+++ model-building/penguins-model-building.py
@@ -1,16 +1,10 @@
 import pandas as pd
-insurance = pd.read_csv('https://github.com/oualid-ben/data/blob/main/clean_data_fraud.csv')
+insurance = pd.read_csv('https://raw.githubusercontent.com/oualid-ben/data/main/clean_data_fraud.csv', on_bad_lines='skip')
 
 # Ordinal feature encoding
 # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
 df = insurance.copy()
-target = 'fraud_reported'
 
-target_mapper = {'Pas de fraud':0, 'Fraude':1}
-def target_encode(val):
-    return target_mapper[val]
-
-df['species'] = df['species'].apply(target_encode)
 
 # Separating X and y
 X = df.drop('fraud_reported', axis=1)
@@ -18,9 +12,11 @@ Y = df['fraud_reported']
 
 # Build random forest model
 from sklearn.ensemble import RandomForestClassifier
+
 clf = RandomForestClassifier()
 clf.fit(X, Y)
 
 # Saving the model
+
 import pickle
-pickle.dump(clf, open('insurance_clf.pkl', 'wb'))
+pickle.dump(clf, open('insurance.pkl', 'wb'))
