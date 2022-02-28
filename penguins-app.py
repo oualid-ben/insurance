5be3fcf40bbc90d7665203e6a5f5fc002f71da6b
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-02-28 17:10:43 +0100

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -15,7 +15,7 @@ Dataset [ins_cl_dataset](https://github.com/oualid-ben/data/blob/main/ins_cl_dat
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/oualid-ben/data/blob/main/ins_cl_dataset.csv)
+[Example CSV input file](https://raw.githubusercontent.com/oualid-ben/data/main/clean_data_example.csv)
 """)
 
 # Collects user input features into dataframe
@@ -24,35 +24,23 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
+        model  = st.sidebar.selectbox('Model',('KNN','Tree decision'))
+        number_of_vehicles_involved = st.sidebar.slider('number of vehicles involved', 1,2,3)
+        witnesses = st.sidebar.slider('Witnesses', 0,1,3)
+        data = {'model': Model,
+                'number_of_vehicles_involved': number_of_vehicles_involved,
+                'witnesses': witnesses,
+               }
         features = pd.DataFrame(data, index=[0])
         return features
     input_df = user_input_features()
 
 # Combines user input features with entire penguins dataset
 # This will be useful for the encoding phase
-penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'], axis=1)
-df = pd.concat([input_df,penguins],axis=0)
+insurance_raw = pd.read_csv('https://raw.githubusercontent.com/oualid-ben/data/main/clean_data.csv')
+opt = input_df.drop(columns=['Model'], axis=1)
+df = pd.concat([input_df,opt],axis=0)
 
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
 df = df[:1] # Selects only the first row (the user input data)
 
 # Displays the user input features
@@ -73,8 +61,8 @@ prediction_proba = load_clf.predict_proba(df)
 
 
 st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
+insurance_species = np.array(['Yes','No'])
+st.write(insurance_species[prediction])
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
