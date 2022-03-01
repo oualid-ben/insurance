3784e76e67f7109495da037e33230f567c765efc
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-03-01 17:00:05 +0100

    Update insurance-app.py

--- insurance-app.py
+++ insurance-app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 st.write("""
 # Insurance fraud detection: IMAFA
