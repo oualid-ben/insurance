8630ee5ae1d1bd7d70e50e6deebc23e3facd03b2
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-03-01 17:26:37 +0100

    Update insurance-app.py

--- insurance-app.py
+++ insurance-app.py
@@ -75,7 +75,7 @@ else:
     st.write(df)
 
 # Reads in saved classification model
-load_clf = pickle.load(open('insurance_forest.pkl', 'rb'))
+load_clf = pickle.load(open('insurance.pkl', 'rb'))
 
 # Apply model to make predictions
 prediction = load_clf.predict(df)
