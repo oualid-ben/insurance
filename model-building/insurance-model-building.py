f17064ccd5557da0e59e3b2592af9e78ee2eaead
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-03-01 13:11:08 +0100

    knn bug fixed

--- model-building/insurance-model-building.py
+++ model-building/insurance-model-building.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 insurance = pd.read_csv('https://raw.githubusercontent.com/oualid-ben/data/main/clean_data_fraud.csv', on_bad_lines='skip')
 
 # Ordinal feature encoding
@@ -10,13 +12,16 @@ df = insurance.copy()
 X = df.drop('fraud_reported', axis=1)
 Y = df['fraud_reported']
 
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
+        
+        
 # Build random forest model
 from sklearn.neighbors import KNeighborsClassifier
 
-knn = KNeighborsClassifier(n_neighbors = 30)
-knn.fit(X, Y)
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, y_train)
+
 
 # Saving the model
-
 import pickle
 pickle.dump(knn, open('insurance.pkl', 'wb'))
